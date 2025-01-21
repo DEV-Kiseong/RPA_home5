fcb8830b70909db0cb87ce105b274db6c15a8701
Kiseong Lee <148989349+DEV-Kiseong@users.noreply.github.com>
2025-01-21 18:17:40 +0900

    Add files via upload

--- neighbor.py
+++ neighbor.py
@@ -1,63 +1,33 @@
-from selenium import webdriver # 동적 사이트 수집
-from selenium.webdriver.chrome.service import Service             # 자동적 접근
-from selenium.webdriver.chrome.options import Options             # 크롭 드라이버 옵션 지정
-from selenium.webdriver.common.by import By                       # find_element 함수 사용을 위해
+from selenium import webdriver                                      # 동적 사이트 수집
+from selenium.webdriver.chrome.service import Service               # 자동적 접근
+from selenium.webdriver.chrome.options import Options               # 크롭 드라이버 옵션 지정
+from selenium.webdriver.common.keys import Keys                     # actions key 사용
+from selenium.webdriver.common.by import By                         # find_element 함수 사용을 위해
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime,timedelta
 
 import time
-import logging
+import pyperclip
+import pyautogui
+
 chrome_options=Options()
 chrome_service=Service()
 chrome_options.add_experimental_option("detach", True)                              # 브라우저 꺼짐 방지 옵션(driver위에 위치해야 한다.)
 chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])       # 불필요한 에러 메세지 없애기
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])    # 자동화 메시지 끄기
+prefs = {
+    "credentials_enable_service": False,                                                        # 비밀번호 자동저장 알럿 닫기
+    "profile.password_manager_enabled": False,                                                  # 비밀번호 자동저장 알럿 닫기
+}
+chrome_options.add_experimental_option("prefs", prefs)                                    # 위에 prefs 설정
 
 driver = webdriver.Chrome(service=chrome_service,options=chrome_options)                        # 크롬 드라이버 실행(설치불필요 ver.4 이상)
 
-# 세팅 클래스
-class setting_up():
-    # 스크린샷 세팅 함수
-    def screenshot_method(self, external_call=False):
-        screenshot_path = 'C:\\Users\\Daou\\PycharmProjects\\screenshot\\screenshot' + datetime.today().strftime("%Y%m%d_%H%M%S") + '.png'
-        driver.save_screenshot(screenshot_path)
-        try:
-            if external_call == False:
-                self.logger.info("특이사항 발생 시 스크린샷 촬영 및 저장합니다.")
-            else:
-                self.logger.error("!!!!! 특이사항 감지하여 스크린샷 촬영 및 저장")
-        except Exception as e:
-            self.logger.error(f'스크린샷 저장실패: {e}')
-
-    # 로그 세팅 함수
-    def logging_method(self):
-        self.logger = logging.getLogger("add_neighbor")
-        self.logger.setLevel(level=logging.DEBUG)  # Output level //DEBUG, INFO, WARNING, ERROR, CRITICAL
-        self.user_id = "userID"
-
-        formatter = logging.Formatter("[%(asctime)s] %(levelname)s %(name)s " + self.user_id + "[%(thread)d] [%(funcName)s : line %(lineno)d] - %(message)s")
-        # 콘솔 출력
-        stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(logging.DEBUG)                   # 콘솔에만 DEBUG 이상 로그 표시
-        stream_handler.setFormatter(formatter)
-
-        # 파일에 출력
-        self.logfile = r'D:\파이썬\RPA_home\log\log' + datetime.today().strftime("%Y%m%d_%H%M%S") + '.log'  # 로그파일 주소
-        file_handler = logging.FileHandler(self.logfile, 'w', encoding="UTF-8")
-        file_handler.setLevel(logging.INFO)                     # 파일에만  INFO 이상 로그 표시
-        file_handler.setFormatter(formatter)
-
-        # 중복 핸들러 추가 방지
-        if self.logger.hasHandlers():
-            self.logger.handlers.clear()
-        # 핸들러 추가
-        self.logger.addHandler(stream_handler)
-        self.logger.addHandler(file_handler)
-        self.logger.info("*************** 자동화(RPA) 로그 시 작 ***************")  # 첫번째 로그 메시지
 # 이웃 추가 클래스
 class neighbor():
+    actions = ActionChains(driver)
     # 로그인 함수
     def login(self):
         driver.implicitly_wait(30)                                              # 웹페이지 로딩 될때까지 5초 wait
@@ -69,9 +39,8 @@ class neighbor():
         login_button = WebDriverWait(driver,10).until(
             EC.element_to_be_clickable((By.XPATH,login_button_xpath))
         )
-        actions = ActionChains(driver)
-        actions.move_to_element(login_button).click().perform()
-        sup.logger.info("네이버 홈페이지에서 로그인 버튼 클릭 PASS")
+        self.actions.move_to_element(login_button).click().perform()
+        print("네이버 홈페이지에서 로그인 버튼 클릭 PASS")
         login_info = {
                     "id" :'//*[@id="id"]',
                     "password" : '//*[@id="pw"]',
@@ -79,35 +48,63 @@ class neighbor():
                 }
         user_id = "lksday62"
         user_pw = "@navks9562"
-        try:
-            for login_key, login_value in login_info.items():
+        for login_key, login_value in login_info.items():
+            if login_key == "login":
+                path = driver.find_element(By.XPATH, login_value)
+                self.actions.move_to_element(path).click().perform()
+                time.sleep(1)
+                print("로그인 버튼 클릭 PASS")
+            else :
+                path = driver.find_element(By.XPATH, login_value)
+                self.actions.move_to_element(path).click().perform()
                 if login_key == "id":
-                    path = driver.find_element(By.XPATH, login_value)
-                    actions.move_to_element(path).click().perform()
-                    actions.send_keys(user_id).perform()
+                    pyperclip.copy(user_id)
+                    path.send_keys(Keys.CONTROL,'v')
                     time.sleep(1)
-                    sup.logger.info(f'{login_key}입력 PASS')
-                elif login_key == "password" :
-                    path = driver.find_element(By.XPATH, login_value)
-                    actions.move_to_element(path).click().perform()
-                    actions.send_keys(user_pw).perform()
-                    sup.logger.info(f'{login_key} 입력 PASS')
-                elif login_key == "login":
-                    path = driver.find_element(By.XPATH, login_value)
-                    actions.move_to_element(path).click().perform()
+                else :
+                    pyperclip.copy(user_pw)
+                    path.send_keys(Keys.CONTROL,'v')
                     time.sleep(1)
-                    sup.logger.info("로그인 버튼 클릭 PASS")
-            sup.logger.info("로그인 완료 PASS")
+                print(f'{login_key} 입력 PASS')
+        # not_ask_again = driver.find_element(By.ID,"wait")           # 이 브라우저에서 2단계인증 넘어가기 체크
+        # actions.move_to_element(not_ask_again).click().perform()
+        try :
+            otp_btn = driver.find_element(By.ID,'useotpBtn')
+            self.actions.move_to_element(otp_btn).click().perform()
+            print("OTP 사용버튼 클릭 PASS")
         except:
-            sup.logger.error("!!!!! 네이버 로그인 실패 ERROR")
+            print("OTP 사용버튼 클릭 실패 ERROR")
 
+        OTP = input("OTP 인증번호 입력: ")
+
+        otp_input_box = driver.find_element(By.ID,"otp")
+        self.actions.move_to_element(otp_input_box).click().perform()
+        self.actions.send_keys(OTP).perform()
+
+        confirm = driver.find_element(By.ID,"confirm")
+        self.actions.move_to_element(confirm).click().perform()
+
+        print("로그인 완료 PASS")
+    def search(self):
+        url = "https://www.naver.com/"                                          # 네이버 접속
+        driver.get(url)
+        search_box = driver.find_element(By.ID,"query")
+        self.actions.move_to_element(search_box).click().perform()
+        time.sleep(1)
+        search_keyword = input("키워드 입력: ")
+        time.sleep(1)
+        self.actions.send_keys(search_keyword).perform()
+        time.sleep(1)
+        search_box.send_keys(Keys.ENTER)
+        time.sleep(2)
+        blog_tap_xpath = '//*[@id="lnb"]//*[contains(text(),"블로그")]'
+        blog = driver.find_element(By.XPATH,blog_tap_xpath)
+        self.actions.move_to_element(blog).click().perform()
+
+# //*[@id="lnb"]/div[1]/div/div[1]/div/div[1]/div[2]/a/text()
 
 # =================== 클래스 및 함수 ==========================
-sup = setting_up()
-sup.logging_method()
-sup.screenshot_method()
-
 nb = neighbor()
-nb.login()
-
+# nb.login()
+nb.search()
 # driver.quit()
